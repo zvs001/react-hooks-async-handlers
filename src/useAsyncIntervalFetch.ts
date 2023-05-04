edf3172ffdddc1ef9d1b93004a7ef8fa1998fbd0
Vlad_Zainchkovskyi <zvsx001@gmail.com>
2023-05-05 01:48:02 +0300

    fix(useAsyncIntervalFetch): fix issue when interval is called twice

--- src/useAsyncIntervalFetch.ts
+++ src/useAsyncIntervalFetch.ts
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React from 'react'
 import { useInterval } from 'usehooks-ts'
 import useAsyncFetch, { FetchActionOptions } from './useAsyncFetch'
 
@@ -8,20 +8,13 @@ function useAsyncIntervalFetch(
   options?: FetchActionOptions,
   dependencies?: React.DependencyList,
 ) {
-  const [intervalCount, setIntervalCount] = useState(0)
-  const depsList = useMemo(() => {
-    let list: React.DependencyList = [intervalCount]
-    list = list.concat(dependencies || [])
-    return list
-  }, [intervalCount, dependencies])
-  const fetchAction = useAsyncFetch(fnFetch, options, depsList)
+  const fetchAction = useAsyncFetch(fnFetch, options, dependencies)
 
   const isIntervalEnabled = !fetchAction.isLoading
 
   useInterval(
     () => {
       fetchAction.reset()
-      setIntervalCount(intervalCount + 1)
     },
     isIntervalEnabled ? millis : null,
   )
